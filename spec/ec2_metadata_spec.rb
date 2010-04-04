1c4df60b9f612d2f71aaef4a284f61aaedb9a073
akimatter <akm2000@gmail.com>
2010-04-04 12:24:17 +0900

    リビジョン指定の属性の取得もオッケーになりました。

--- spec/ec2_metadata_spec.rb
+++ spec/ec2_metadata_spec.rb
@@ -58,18 +58,21 @@ describe Ec2Metadata do
 #       obj[:openssh_key].should == "ssh-rsa 1234567890"
 #     end
 
-#     REVISIONS.each do |rev|
-#       describe "with revision #{rev}" do
-#         it "('#{rev}')[attr_name] should return value of respose for http://169.254.169.254/#{rev}/meta-data/attr_name" do
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
-#           ATTR_NAMES.each do |attr_name|
-#             Net::HTTP.should_receive(:get).with("169.254.169.254", "#{rev}/meta-data/#{attr_name}").and_return("#{rev}_#{attr_name}")
-#             Ec2Metadata[rev][attr_name].should == "#{rev}_#{attr_name}"
-#             Ec2Metadata[rev.to_sym][attr_name.to_sym].should == "#{rev}_#{attr_name}"
-#           end
-#         end
-#       end
-#     end
+    REVISIONS.each do |rev|
+      describe "with revision #{rev}" do
+        it "('#{rev}')[attr_name] should return value of respose for http://169.254.169.254/#{rev}/meta-data/attr_name" do
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{rev}/").once.and_return(DATA_TYPES.join("\n"))
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{rev}/meta-data/").once.and_return(ATTR_NAMES.join("\n"))
+
+          ATTR_NAMES.each do |attr_name|
+            Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{rev}/meta-data/#{attr_name}").once.and_return("#{rev}_#{attr_name}")
+            Ec2Metadata[rev][attr_name].should == "#{rev}_#{attr_name}"
+            Ec2Metadata[rev.to_sym][attr_name.to_sym].should == "#{rev}_#{attr_name}"
+          end
+        end
+      end
+    end
 
   end
   
