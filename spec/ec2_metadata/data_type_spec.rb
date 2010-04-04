3d83cd45d5b0d718ccab29012e67b13defb9e242
akimatter <akm2000@gmail.com>
2010-04-04 15:57:38 +0900

    /meta-data/block-device-mappingのテストを追加しました。

--- spec/ec2_metadata/data_type_spec.rb
+++ spec/ec2_metadata/data_type_spec.rb
@@ -29,20 +29,19 @@ describe Ec2Metadata::DataType do
           obj[:availability_zone].should == "us-east-1a"
         end
 
-#         it "('block_device_mapping') should return object like Hash" do
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/").and_return(["ami", "ephemeral0", "root", "swap"].join("\n"))
-#           obj = Ec2Metadata[:block_device_mapping]
-#           obj.keys.should == ["ami", "ephemeral0", "root", "swap"]
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ami").and_return("sda1")
-#           obj[:ami].should == "sda1"
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ephemeral0").and_return("sda2")
-#           obj[:ephemeral0].should == "sda2"
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/root").and_return("/dev/sda1")
-#           obj[:root].should == "/dev/sda1"
-#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/swap").and_return("sda3")
-#           obj[:swap].should == "sda3"
-#         end
+        it "('block_device_mapping') should return object like Hash" do
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/block-device-mapping/").and_return(["ami", "ephemeral0", "root", "swap"].join("\n"))
+          obj = @meta_data[:block_device_mapping]
+          obj.child_names.should == ["ami", "ephemeral0", "root", "swap"]
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/block-device-mapping/ami").and_return("sda1")
+          obj[:ami].should == "sda1"
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/block-device-mapping/ephemeral0").and_return("sda2")
+          obj[:ephemeral0].should == "sda2"
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/block-device-mapping/root").and_return("/dev/sda1")
+          obj[:root].should == "/dev/sda1"
+          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/block-device-mapping/swap").and_return("sda3")
+          obj[:swap].should == "sda3"
+        end
 
 #         it "('public_keys') should return object like Hash" do
 #           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
