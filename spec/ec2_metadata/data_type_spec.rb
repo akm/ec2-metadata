fb2f1dd5933a8e3a6362b3364cbe4f702da67913
akimatter <akm2000@gmail.com>
2010-04-04 15:44:30 +0900

    placement, block_device_mapping, public_keysのテストをspec/ec2_metadata/data_type_spec.rbに移しました。

--- spec/ec2_metadata/data_type_spec.rb
+++ spec/ec2_metadata/data_type_spec.rb
@@ -5,18 +5,58 @@ describe Ec2Metadata::DataType do
   describe :[] do
 
     REVISIONS.each do |revision|
-      ATTR_NAMES.each do |attr_name|
-        it "(#{attr_name.gsub(/-/, '_').inspect}) should return #{attr_name}" do
-          @meta_data = Ec2Metadata::DataType.new("/#{revision}/meta-data/")
-          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/").once.
-            and_return(ATTR_NAMES.join("\n"))
-          Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/#{attr_name}").once.
-            and_return("#{revision}_#{attr_name}")
-          @meta_data[attr_name].should == "#{revision}_#{attr_name}"
-          @meta_data[attr_name.to_sym].should == "#{revision}_#{attr_name}"
+      describe revision do
+        ATTR_NAMES.each do |attr_name|
+          it "(#{attr_name.gsub(/-/, '_').inspect}) should return #{attr_name}" do
+            @meta_data = Ec2Metadata::DataType.new("/#{revision}/meta-data/")
+            Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/").once.
+              and_return(ATTR_NAMES.join("\n"))
+            Net::HTTP.should_receive(:get).with("169.254.169.254", "/#{revision}/meta-data/#{attr_name}").once.
+              and_return("#{revision}_#{attr_name}")
+            @meta_data[attr_name].should == "#{revision}_#{attr_name}"
+            @meta_data[attr_name.to_sym].should == "#{revision}_#{attr_name}"
+          end
         end
+
+#         it "('placement') should return object like Hash" do
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/placement/").and_return("availability-zone")
+#           obj = Ec2Metadata[:placement]
+#           obj.keys.should == ["availability_zone"]
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/placement/availability-zone").and_return("us-east-1a")
+#           obj[:availability_zone].should == "us-east-1a"
+#         end
+
+#         it "('block_device_mapping') should return object like Hash" do
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/").and_return(["ami", "ephemeral0", "root", "swap"].join("\n"))
+#           obj = Ec2Metadata[:block_device_mapping]
+#           obj.keys.should == ["ami", "ephemeral0", "root", "swap"]
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ami").and_return("sda1")
+#           obj[:ami].should == "sda1"
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ephemeral0").and_return("sda2")
+#           obj[:ephemeral0].should == "sda2"
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/root").and_return("/dev/sda1")
+#           obj[:root].should == "/dev/sda1"
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/swap").and_return("sda3")
+#           obj[:swap].should == "sda3"
+#         end
+
+#         it "('public_keys') should return object like Hash" do
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/").and_return("0=keypair0")
+#           obj = Ec2Metadata[:public_keys]
+#           obj.to_s.should == "keypair0"
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/o/").and_return("openssh-key")
+#           obj.attr_names.should == ["openssh-key"]
+#           Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/o/openssh-key").and_return("ssh-rsa 1234567890")
+#           obj[:openssh_key].should == "ssh-rsa 1234567890"
+#         end
+
+
       end
     end
+
 
   end
   
--- spec/ec2_metadata_spec.rb
+++ spec/ec2_metadata_spec.rb
@@ -7,11 +7,6 @@ describe Ec2Metadata do
       Ec2Metadata.clear_instance
     end
 
-# irb(main):068:0> Net::HTTP.get(DEFAULT_HOST, "/latest/dynamic/").split(/$/).map(&:strip)
-# => [""]
-# irb(main):069:0> Net::HTTP.get(DEFAULT_HOST, "/latest/user-data/").split(/$/).map(&:strip)
-# => ["<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"", "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">", "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">", "<head>", "<title>404 - Not Found</title>", "</head>", "<body>", "<h1>404 - Not Found</h1>", "</body>", "</html>", ""]
- 
     ATTR_NAMES.each do |attr_name|
       it "(#{attr_name.gsub(/-/, '_')}) should return value of respose for http://169.254.169.254/latest/meta-data/#{attr_name}" do
         Net::HTTP.should_receive(:get).with("169.254.169.254", "/").once.and_return(REVISIONS.join("\n"))
@@ -23,40 +18,6 @@ describe Ec2Metadata do
       end
     end
 
-#     it "('placement') should return object like Hash" do
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/placement/").and_return("availability-zone")
-#       obj = Ec2Metadata[:placement]
-#       obj.keys.should == ["availability_zone"]
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/placement/availability-zone").and_return("us-east-1a")
-#       obj[:availability_zone].should == "us-east-1a"
-#     end
-
-#     it "('block_device_mapping') should return object like Hash" do
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/").and_return(["ami", "ephemeral0", "root", "swap"].join("\n"))
-#       obj = Ec2Metadata[:block_device_mapping]
-#       obj.keys.should == ["ami", "ephemeral0", "root", "swap"]
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ami").and_return("sda1")
-#       obj[:ami].should == "sda1"
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/ephemeral0").and_return("sda2")
-#       obj[:ephemeral0].should == "sda2"
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/root").and_return("/dev/sda1")
-#       obj[:root].should == "/dev/sda1"
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/block-device-mapping/swap").and_return("sda3")
-#       obj[:swap].should == "sda3"
-#     end
-
-#     it "('public_keys') should return object like Hash" do
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "/").and_return(REVISIONS.join("\n"))
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/").and_return("0=keypair0")
-#       obj = Ec2Metadata[:public_keys]
-#       obj.to_s.should == "keypair0"
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/o/").and_return("openssh-key")
-#       obj.attr_names.should == ["openssh-key"]
-#       Net::HTTP.should_receive(:get).with("169.254.169.254", "latest/meta-data/public-keys/o/openssh-key").and_return("ssh-rsa 1234567890")
-#       obj[:openssh_key].should == "ssh-rsa 1234567890"
-#     end
 
     REVISIONS.each do |rev|
       describe "with revision #{rev}" do
